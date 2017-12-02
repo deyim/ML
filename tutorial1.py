ecc67d757ec8f4229eb21876832471a8189cbe13
da-eun-yim <deyim33@gmail.com>
2017-12-02 13:30:40 +0900

    tensorflow regression

---
+++ tutorial1.py
@@ -0,0 +1,25 @@
+import tensorflow as tf
+
+
+
+node1 = tf.constant(3.0, tf.float32)
+node2 = tf.constant(4.0)
+node3 = tf.add(node1, node2)
+
+print("node1: ", node1, "node2: ", node2)
+print("node3: ", node3)
+
+sess = tf.Session()
+print("sess run(node1, node2):" ,sess.run([node1, node2]))
+print("sess.run(node2): ", sess.run(node3))
+print()
+
+a = tf.placeholder(tf.float32)
+b = tf.placeholder(tf.float32)
+adder_node = a+b
+
+print(sess.run(adder_node, feed_dict={a: 3, b: 4.5}))
+print(sess.run(adder_node, feed_dict={a: [1,3], b: [2,4]}))
+#______________How to start Tensor Flow_________
+#make graph
+#and then run the session
